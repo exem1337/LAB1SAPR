de1f7ddce5112895f5dbbc04616c1bb28e8e99bc
alexander.chernyakov <alexander.chernyakov@waveaccess.global>
2023-10-23 01:05:56 +0500

    final model and rectangle function

--- LAB1SAPR/main.cpp
+++ LAB1SAPR/main.cpp
@@ -33,6 +33,52 @@ void line(float startX, float startY, float endX, float endY) {
 	UF_CURVE_create_line(&line_coords, &entid);
 }
 
+void rectangle(float startX, float startY, float sideWitdh) {
+	// функция строит квадрат от верхей левой его точки
+		
+	tag_t entid = 0;
+	UF_CURVE_line_t line1_coords;
+	UF_CURVE_line_t line2_coords;
+	UF_CURVE_line_t line3_coords;
+	UF_CURVE_line_t line4_coords;
+
+	//верхняя линия
+	line1_coords.start_point[0] = startX;// X1
+	line1_coords.start_point[1] = startY;// Y1
+	line1_coords.start_point[2] = 0.;// Z1
+	line1_coords.end_point[0] = startX + sideWitdh;// X2
+	line1_coords.end_point[1] = startY;// Y2
+	line1_coords.end_point[2] = 0.;// Z2
+	UF_CURVE_create_line(&line1_coords, &entid);
+
+	//правая линия
+	line2_coords.start_point[0] = startX + sideWitdh;// X1
+	line2_coords.start_point[1] = startY;// Y1
+	line2_coords.start_point[2] = 0.;// Z1
+	line2_coords.end_point[0] = startX + sideWitdh;// X2
+	line2_coords.end_point[1] = startY - sideWitdh;// Y2
+	line2_coords.end_point[2] = 0.;// Z2
+	UF_CURVE_create_line(&line2_coords, &entid);
+
+	//нижняя линия
+	line3_coords.start_point[0] = startX;// X1
+	line3_coords.start_point[1] = startY - sideWitdh;// Y1
+	line3_coords.start_point[2] = 0.;// Z1
+	line3_coords.end_point[0] = startX + sideWitdh;// X2
+	line3_coords.end_point[1] = startY - sideWitdh;// Y2
+	line3_coords.end_point[2] = 0.;// Z2
+	UF_CURVE_create_line(&line3_coords, &entid);
+
+	//левая линия
+	line4_coords.start_point[0] = startX;// X1
+	line4_coords.start_point[1] = startY;// Y1
+	line4_coords.start_point[2] = 0.;// Z1
+	line4_coords.end_point[0] = startX;// X2
+	line4_coords.end_point[1] = startY - sideWitdh;// Y2
+	line4_coords.end_point[2] = 0.;// Z2
+	UF_CURVE_create_line(&line4_coords, &entid);
+}
+
 void ufusr(char* param, int* retcode, int paramLen)
 {
 	if (UF_initialize()) return;
@@ -48,8 +94,21 @@ void ufusr(char* param, int* retcode, int paramLen)
 	line(115., 50., 0., 50.);
 	line(0., 50., 0., 0.);
 
-	line(29., 0., 29., 50.);
-	line(83., 0., 83., 50.);
+	line(27., 0., 27., 10.);
+	line(27., 50., 27., 40.);
+	line(81., 0., 81., 50.);
+
+	// левый квадрат
+	line(10., 10., 10., 40.);
+	line(10., 10., 34., 10.);
+	line(10., 40., 34., 40.);
+	line(34., 40., 34., 10.);
+
+	//центральный большой квадрат
+	rectangle(34., 45., 40.);
+
+	//центральный малый квадрат
+	rectangle(39., 40., 30.);
 
 	UF_terminate();
 }
