5c498b01ea895a793b79182f2282e4524ba0ad64
alexander.chernyakov <alexander.chernyakov@waveaccess.global>
2023-12-27 12:25:14 +0500

    new lab

--- LAB1SAPR/main.cpp
+++ LAB1SAPR/main.cpp
@@ -203,93 +203,78 @@ void lab2() {
 }
 }
 
-void doSomeShit() {
-	char a[] = "30.0";
-	char b[] = "80.0";
-	char t_a[] = "0.0";
-	/* Определяется массив линий с координатами их
-концов, используемых в эскизе выдавливаемого сечения */
-	UF_CURVE_line_t line[3] = {
-	{{10.0,0.0,0.0}, {10.0,40.0,0.0}},
-	{{10.0,0.0,0.0}, {40.0,0.0,0.0}},
-	{{10.0,40.0,0.0}, {40.0,40.0,0.0}} };
-	UF_CURVE_arc_t arc; //структура данных дуги
-	tag_t objarray[4], //теги для трех линий и дуги
-		wcs_tag; //тег для системы координат
-	int i;
-	//параметры операции выдавливания
-	//расстояние начала и расстояние окончания выдавливания
-
-		char* limit[2] = { a, b };
-	//орты вектора направления выдавливания
-	double direction[3] = { 0.0, 0.0, 1.0 };
-	//признак создания самостоятельного тела
-	UF_FEATURE_SIGN create = UF_NULLSIGN;
-	//заготовки указателей на перечии объектов
-	uf_list_p_t loop_list, //Список объектов, подлежащих выдавливанию (элементы сечения)
-		features; //Список созданных идентификаторов объектов
-	//угол конусности в градусах
-	char* taper_angle = t_a;
-	//параметр не используется
-	double ref_pt[3];
-
-		// задание пераметров дуги в сечении
-		arc.start_angle = -90.0 * DEGRA;
-		arc.end_angle = 90.0 * DEGRA;
-		arc.arc_center[0] = 40.0;
-		arc.arc_center[1] = 20.0;
-		arc.arc_center[2] = 0.0;
-		arc.radius = 20.0;
-		for (i = 0; i < 3; i++) //построение трех линий сечения
-			UF_CURVE_create_line(&line[i], &objarray[i]);
-		//получение матрицы абсолютных координат
-		UF_CSYS_ask_wcs(&wcs_tag);
-		//связывание матрицы поворота дуги с абсолютными координатами
-		UF_CSYS_ask_matrix_of_object(wcs_tag, &arc.matrix_tag);
-		//построение дуги сечения
-		UF_CURVE_create_arc(&arc, &objarray[i]);
-		//создание пустого перечия объектов
-		UF_MODL_create_list(&loop_list);
-		//заполнение перечия тремя линиями и одной дугой
-		for (i = 0; i < 4; i++)
-			UF_MODL_put_list_item(loop_list, objarray[i]);
+void lab2Serega() {
+	int degree = 3; // степень сплайна
+	int periodicity = 0; // сплайн не периодичный
+	int num_points = 11;
+	int save_def_data = 1;
+	tag_t spline_tag;
 	
-			//создание операции выдавливания
-			UF_MODL_create_extruded(loop_list, taper_angle, limit,
-				ref_pt, direction, create, &features);
+	UF_CURVE_pt_slope_crvatr_t point_data[11]
+	{
+		{ {0.0, 0.0, 0},
+		UF_CURVE_SLOPE_DIR, { 0.0000, 0.0000, 0.0000 },
+		UF_CURVE_CRVATR_NONE, { 0.0000, 0.0000, 0.0000 }
+		},
+		{ {-112, 14, 0},
+		UF_CURVE_SLOPE_NONE, {0.0000, 0.0000, 0.0000},
+		UF_CURVE_CRVATR_NONE, {0.0000, 0.0000, 0.0000}
+		},
+		{ {-224, 51, 0},
+		UF_CURVE_SLOPE_NONE, { 0.0000, 0.0000, 0.0000 },
+		UF_CURVE_CRVATR_NONE, { 0.0000, 0.0000, 0.0000 }
+		},
+		{ {-336, 97, 0},
+		UF_CURVE_CRVATR_NONE, { 0.0000, 0.0000, 0.0000 },
+		UF_CURVE_CRVATR_NONE, { 0.0000, 0.0000, 0.0000 }
+		},
+		{ {-448, 135, 0},
+		UF_CURVE_SLOPE_DIR,{0.0000, 0.0000, 0.0000},
+		UF_CURVE_CRVATR_NONE,{0.0000, 0.0000, 0.0000}
+		},
+		{ {-560, 149, 0},
+		UF_CURVE_CRVATR_NONE, { 0.0000, 0.0000, 0.0000 },
+		UF_CURVE_CRVATR_NONE, { 0.0000, 0.0000, 0.0000 }
+		},
+		{ {-672, 135, 0},
+		UF_CURVE_CRVATR_NONE, { 0.0000, 0.0000, 0.0000 },
+		UF_CURVE_CRVATR_NONE, { 0.0000, 0.0000, 0.0000 }
+		},
+		{ {-784, 97, 0},
+		UF_CURVE_CRVATR_NONE, { 0.0000, 0.0000, 0.0000 },
+		UF_CURVE_CRVATR_NONE, { 0.0000, 0.0000, 0.0000 }
+		},
+		{ {-896, 51, 0},
+		UF_CURVE_CRVATR_NONE, { 0.0000, 0.0000, 0.0000 },
+		UF_CURVE_CRVATR_NONE, { 0.0000, 0.0000, 0.0000 }
+		},
+		{ {-1008, 14, 0},
+		UF_CURVE_CRVATR_NONE, { 0.0000, 0.0000, 0.0000 },
+		UF_CURVE_CRVATR_NONE, { 0.0000, 0.0000, 0.0000 }
+		},
+		{ {-1120, 0.0, 0},
+		UF_CURVE_CRVATR_NONE, { 0.0000, 0.0000, 0.0000 },
+		UF_CURVE_CRVATR_NONE, { 0.0000, 0.0000, 0.0000 }
+		}
+	};
+
+	if (!UF_initialize())
+	{
+		// создание сплайна
+		UF_CURVE_create_spline_thru_pts(degree,
+			periodicity,
+			num_points,
+			point_data,
+			NULL, //с заданием монотонности по умолчанию
+			save_def_data,
+			&spline_tag);
+		line(0., -73., 0, 0);
+		line(0., -73., -1120., -73.);
+		line(-1120., -73., -1120., 0.);
+		UF_terminate();
+	}
 }
 
-void labserega() {
-	line(0.0, 0.0, 100, 0);
-	line(0.0, 10, 100, 10);
-
-	line(100, 0, 100, 10);
-	line(100, 10, 92.5, 10);
-	line(92.5, 10, 75, 80);
-
-	line(75, 80, 67.5, 80);
-	line(25, 80, 32.5, 80);
-
-	line(25, 80, 7.5, 10);
-	line(7.5, 10, 0, 10);
-	line(0, 10, 0, 0);
-
-	createArc(
-		new double[3] { 50, 80, 0 },
-		25,
-		180,
-		360
-	);
-
-	createArc(
-		new double[3] { 50, 80, 0 },
-		17.5,
-		180,
-		360
-	);
-
-	circle(50, 40, 7.5);
-}
 
 void lab3() {
 	UF_FEATURE_SIGN sign = UF_NULLSIGN;
@@ -443,9 +428,6 @@ void ShpereDelete(tag_t* next_tag)
 					count++;
 				}
 			}
-
-
-
 		}
 
 	} while (*next_tag != NULL_TAG);
@@ -458,25 +440,12 @@ void ShpereDelete(tag_t* next_tag)
 			cnt = features_node[j]->feat_count;
 			tipN = features_node[j]->feat_type;
 
-
-			//выбрать по типу элемента процедуру для его обработки
-			//if (strcmp(tip, "SPHERE") == 0)
 			if (strcmp(tipN, "SPHERE") == 0)
 				UF_OBJ_delete_object(tags[i]);
 
-			/*if (strcmp(tip, "BLEND") == 0)
-				BlendSub(features_node[i]->feat_tags, cnt);
-			if (strcmp(tip, "CYLINDER") == 0)
-				CylinderSub(features_node[i]->feat_tags, cnt);*/
-
-		} //освободить память от динамической переменной
-		//UF_free(features_node);
-
+		} 
 	}
 	UF_free(features_node);
-	/*sprintf_s(mes, "%d", count);
-	UF_UI_write_listing_window(mes);*/
-
 }
 
 void lab3Serega() {
@@ -493,178 +462,56 @@ void lab3Serega() {
 	int body_type;
 	tag_t body, view;
 	tag_t next_tag = NULL_TAG;
-	//tag_t WorkPartTag = UF_PART_ask_display_part();
-	/*Главная точка входа*/
+
 	if (!UF_initialize()) {//заполнение структуры для функции выбора тела
 		opts.num_mask_triples = 1;
 		opts.mask_triples = &mask;
 		opts.scope = UF_UI_SEL_SCOPE_WORK_PART;
 		//активизация информационного окна системы для вывода текста
 		UF_UI_open_listing_window();
-		//начать цикл
-		//do {//открыть диалог выбора тела
+	
 		err = UF_UI_select_single(msg, &opts, &response, &body, cursor, &view);
 		//если на диалоге нажата кнопка ОК или ПРИНЯТЬ,
 		if ((response == 4) || (response == 5))
 		{
-
-
 			ShpereDelete(&next_tag);
-
-			/*if (tip == "SPHERE")
-				SphereSub(features_node[i]->feat_tags, cnt);*/
-				/*if (strcmp(tip, "BLEND") == 0)
-					BlendSub(features_node[i]->feat_tags, cnt);
-				if (strcmp(tip, "CYLINDER") == 0)
-					CylinderSub(features_node[i]->feat_tags, cnt);*/
-
-					//убрать цвет выделения с выбранного тела сцены
-					//UF_DISP_set_highlight(body, 0);
-
-				//продолжать цикл, пока пользователь нажимает кнопки ОК или ПРИНЯТЬ
 		}
-		/*} while ((response == 4) || (response == 5));*/
 		UF_terminate();
 	}
 }
 
-//void lab2Serega() {
-//	int degree = 3; // степень сплайна
-//	int periodicity = 0; // сплайн не периодичный
-//	int num_points = NUMBER_POINTS;
-//	int save_def_data = 1;
-//	tag_t spline_tag;
-//	
-//	UF_CURVE_pt_slope_crvatr_t point_data[NUMBER_POINTS]
-//	{
-//		{ {0.0, 0.0, 45.0},
-//		UF_CURVE_SLOPE_DIR, { 0.0000, 0.0000, 0.0000 }, 
-//		UF_CURVE_CRVATR_NONE, { 0.0000, 0.0000, 0.0000 }
-//		},
-//		{ {-112, 14, 45.0000},
-//		UF_CURVE_SLOPE_NONE, {0.0000, 0.0000, 0.0000},
-//		UF_CURVE_CRVATR_NONE, {0.0000, 0.0000, 0.0000}
-//		},
-//		{ {-224, 51, 45.0000},
-//		UF_CURVE_SLOPE_NONE, { 0.0000, 0.0000, 0.0000 },
-//		UF_CURVE_CRVATR_NONE, { 0.0000, 0.0000, 0.0000 }
-//		},
-//		{ {-336, 97, 45.0},
-//		UF_CURVE_CRVATR_NONE, { 0.0000, 0.0000, 0.0000 },
-//		UF_CURVE_CRVATR_NONE, { 0.0000, 0.0000, 0.0000 }
-//		},
-//		{ {-448, 135, 45.0},
-//		UF_CURVE_SLOPE_DIR,{0.0000, 0.0000, 0.0000},
-//		UF_CURVE_CRVATR_NONE,{0.0000, 0.0000, 0.0000}
-//		},
-//		{ {-560, 149, 45.0},
-//		UF_CURVE_CRVATR_NONE, { 0.0000, 0.0000, 0.0000 },
-//		UF_CURVE_CRVATR_NONE, { 0.0000, 0.0000, 0.0000 }
-//		},
-//		{ {-672, 135, 45.0},
-//		UF_CURVE_CRVATR_NONE, { 0.0000, 0.0000, 0.0000 },
-//		UF_CURVE_CRVATR_NONE, { 0.0000, 0.0000, 0.0000 }
-//		},
-//		{ {-784, 97, 45.0},
-//		UF_CURVE_CRVATR_NONE, { 0.0000, 0.0000, 0.0000 },
-//		UF_CURVE_CRVATR_NONE, { 0.0000, 0.0000, 0.0000 }
-//		},
-//		{ {-896, 51, 45.0},
-//		UF_CURVE_CRVATR_NONE, { 0.0000, 0.0000, 0.0000 },
-//		UF_CURVE_CRVATR_NONE, { 0.0000, 0.0000, 0.0000 }
-//		},
-//		{ {-1008, 14, 45.0},
-//		UF_CURVE_CRVATR_NONE, { 0.0000, 0.0000, 0.0000 },
-//		UF_CURVE_CRVATR_NONE, { 0.0000, 0.0000, 0.0000 }
-//		},
-//		{ {-1120, 0.0, 45.0},
-//		UF_CURVE_CRVATR_NONE, { 0.0000, 0.0000, 0.0000 },
-//		UF_CURVE_CRVATR_NONE, { 0.0000, 0.0000, 0.0000 }
-//		}
-//	};
-//
-//	for (int i = 0; i < NUMBER_POINTS; ++i) {
-//		double current_slope = point_data[i].slope;
-//		double new_slope = current_slope + angle; // Новый угол наклона
-//
-//		// Предположим, что у вас есть функция для установки угла наклона
-//		set_slope(&point_data[i], new_slope); // Применяем новый угол наклона
-//	}
-//
-//	if (!UF_initialize())
-//	{
-//		// создание сплайна
-//		UF_CURVE_create_spline_thru_pts(degree,
-//			periodicity,
-//			num_points,
-//			point_data,
-//			NULL, //с заданием монотонности по умолчанию
-//			save_def_data,
-//			&spline_tag);
-//		line(0., -73., 0, 0, 0, 45);
-//		line(0., -73., -1120., -73., 0, 0);
-//		line(-1120., -73., -1120., 0., 0, 45);
-//		UF_terminate();
-//	}
-//}
 
-void lab2oleg() {
-	int degree = 5; // степень сплайна
-	int periodicity = 0; // сплайн не периодичный
-	int num_points = NUMBER_POINTS;
-	int save_def_data = 1;
-	tag_t spline_tag;
+void lab1_11var() {
+	line(0, 0, 17.5, 0);
+	line(17.5, 0, 17.5, 25);
+	line(17.5, 25, 52.5, 25);
+	line(52.5, 25, 52.5, 0);
+	line(52.5, 0, 70, 0);
+	line(70, 0, 70, 85);
 
-	// заполнение структуры параметров сплайна
-	UF_CURVE_pt_slope_crvatr_t point_data[NUMBER_POINTS]{
-	  { {0.0, 0.0, 0.0}, // координаты первой точки
-		UF_CURVE_SLOPE_DIR, /*в ней будет задано направление касательной */
-		{ 0.0, 1.0, 0.0000 }, // вектор касательной
-		UF_CURVE_CRVATR_NONE, // кривизна задаваться не будет
-		{ 0.0000, 0.0000, 0.0000 } },
-		// в следующих точках задаются только координаты
-		{ {100.0, 0.0, 0.0}, UF_CURVE_SLOPE_NONE, {0.0000, 0.0000, 0.0000}, UF_CURVE_CRVATR_NONE, {0.0000, 0.0000, 0.0000} },
-		{ {200.0, 100.0, 0.0000}, UF_CURVE_SLOPE_NONE, { 0.0000, 0.0000, 0.0000 }, UF_CURVE_CRVATR_NONE, { 0.0000, 0.0000, 0.0000 } },
-		{ {300.0, 200.0, 0.0}, UF_CURVE_CRVATR_NONE, { 0.0000, 0.0000, 0.0000 }, UF_CURVE_CRVATR_NONE, { 0.0000, 0.0000, 0.0000 } },
-		{ {400.0, 300.0, 0.0}, UF_CURVE_CRVATR_NONE, { 0.0000, 0.0000, 0.0000 }, UF_CURVE_CRVATR_NONE, { 0.0000, 0.0000, 0.0000 } },
-		{ {500.0, 400.0, 0.0}, UF_CURVE_CRVATR_NONE, { 0.0000, 0.0000, 0.0000 }, UF_CURVE_CRVATR_NONE, { 0.0000, 0.0000, 0.0000 } },
-		{ {600.0, 500.0, 0.0}, UF_CURVE_CRVATR_NONE, { 0.0000, 0.0000, 0.0000 }, UF_CURVE_CRVATR_NONE, { 0.0000, 0.0000, 0.0000 } },
-		{ {700.0, 600.0, 0.0}, UF_CURVE_CRVATR_NONE, { 0.0000, 0.0000, 0.0000 }, UF_CURVE_CRVATR_NONE, { 0.0000, 0.0000, 0.0000 } },
-	};
-	// создание сплайна
-	UF_CURVE_create_spline_thru_pts(degree, periodicity, num_points, point_data, NULL, //с заданием монотонности по умолчанию
-		save_def_data, &spline_tag);
-}
-
-void lab1Oleg() {
-	line(0, 0, 120, 0);
-	line(120, 0, 120, 10);
-	line(120, 10, 100, 45);
-	line(100, 45, 95, 45);
-	line(0, 0, 0, 10);
-	line(0, 10, 20, 45);
-	line(20, 45, 25, 45);
-	
-	line(25, 0, 25, 45);
-	line(95, 0, 95, 45);
-
-	circle(60, 45, 20);
-
-	double center[3] = { 60, 45, 0.0 }; // Центр дуги
+	double center[3] = { 35, 85, 0.0 }; // Центр дуги
 	float radius = 35; // Радиус дуги
 	int startAngle = 0; // Стартовый угол в градусах
 	int endAngle = 180; // Конечный угол в градусах
 
 	createArc(center, radius, startAngle, endAngle);
+
+	line(0, 85, 0, 0);
+
+	line(0, 45, 70, 45);
+	line(0, 20, 17.5, 20);
+	line(52.5, 20, 70, 20);
+
+	circle(35, 85, 22.5);
 }
 
 void ufusr(char* param, int* retcode, int paramLen)
 {
 	if (UF_initialize()) return;
 
-	lab1Oleg();
-	//lab2();
-	//lab3();
+	lab1_11var();
+	//lab2Serega();
+	//lab3Serega();
 ;
 	UF_terminate();
 }
